7744e9d7196aed5b2ceeac68075b1393e4900019
KapranvIvan <ivankapranov1998@gmail.com>
2016-04-08 22:51:06 +0300

    Parallax Rework

---
+++ ToTheLight/Assets/BattleScene/Map/ParallaxBackground/LightComponentBackground.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class LightComponentBackground : MonoBehaviour
+{
+
+    float transparence = 1;
+    public SpriteRenderer dark;
+    private Color darkColor;
+    private Color newColor;
+    public Color cl;
+
+
+    public void SetLight(float value)
+    {
+        darkColor = new Color(darkColor.r, darkColor.g, darkColor.b, value);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        darkColor = dark.color;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+    }
+}
--- ToTheLight/Assets/BattleScene/SceneGenerator.cs
+++ ToTheLight/Assets/BattleScene/SceneGenerator.cs
@@ -13,16 +13,35 @@ public class SceneGenerator : MonoBehaviour {
     public GameObject[] forestMidBlocks;
     public GameObject[] forestEndBlocks;
 
-    private GameObject rotate;
+    public GameObject[] forestNightBackground;
+
+    private GameObject b0;
+    private GameObject b1;
+    private GameObject b2;
+    private GameObject b3;
+    private GameObject b4;
+    private GameObject b5;
+    private GameObject b6;
+
 
     private void MapCreator(GameObject[] begin, GameObject[] mid, GameObject[] end)
     {
+        int randomBackground = Random.Range(0, forestNightBackground.Length - 1);
+        b0 = (GameObject)GameObject.Instantiate(forestNightBackground[randomBackground], new Vector3(startPoint.transform.position.x - (float)8.7 * 2.725f, startPoint.transform.position.y + (float)6.5 * 2.45f, 12), startPoint.transform.rotation);
+        b1 = (GameObject)GameObject.Instantiate(forestNightBackground[randomBackground], new Vector3(startPoint.transform.position.x + (float)8.7 * 2.725f, startPoint.transform.position.y + (float)6.5 * 2.45f, 12), startPoint.transform.rotation);
+        b2 = (GameObject)GameObject.Instantiate(forestNightBackground[randomBackground], new Vector3(midPoint1.transform.position.x + (float)8.7 * 2.725f, midPoint1.transform.position.y + (float)6.5 * 2.45f, 12), midPoint1.transform.rotation);
+        b3 = (GameObject)GameObject.Instantiate(forestNightBackground[randomBackground], new Vector3(midPoint2.transform.position.x + (float)8.7 * 2.725f, midPoint2.transform.position.y + (float)6.5 * 2.45f, 12), midPoint2.transform.rotation);
+        b4 = (GameObject)GameObject.Instantiate(forestNightBackground[randomBackground], new Vector3(midPoint3.transform.position.x + (float)8.7 * 2.725f, midPoint3.transform.position.y + (float)6.5 * 2.45f, 12), midPoint3.transform.rotation);
+        b5 = (GameObject)GameObject.Instantiate(forestNightBackground[randomBackground], new Vector3(endPoint.transform.position.x + (float)8.7 * 2.725f, endPoint.transform.position.y + (float)6.5 * 2.45f, 12), endPoint.transform.rotation);
+        b6 = (GameObject)GameObject.Instantiate(forestNightBackground[randomBackground], new Vector3(endPoint.transform.position.x + 47.5f + (float)8.7 * 2.725f, endPoint.transform.position.y + (float)6.5 * 2.45f, 12), endPoint.transform.rotation);
+        b0.GetComponent<MidBlockFlip>().SetFlip();
+        b2.GetComponent<MidBlockFlip>().SetFlip();
+        b4.GetComponent<MidBlockFlip>().SetFlip();
+        b6.GetComponent<MidBlockFlip>().SetFlip();
         GameObject.Instantiate(begin[Random.Range(0, begin.Length - 1)], startPoint.transform.position, startPoint.transform.rotation);
-        rotate = (GameObject)GameObject.Instantiate(mid[Random.Range(0, mid.Length - 1)], midPoint1.transform.position, midPoint1.transform.rotation);
-        rotate.GetComponent<MidFlip>().Flip();
+        GameObject.Instantiate(mid[Random.Range(0, mid.Length - 1)], midPoint1.transform.position, midPoint1.transform.rotation);
         GameObject.Instantiate(mid[Random.Range(0, mid.Length - 1)], midPoint2.transform.position, midPoint2.transform.rotation);
-        rotate = (GameObject)GameObject.Instantiate(mid[Random.Range(0, mid.Length - 1)], midPoint3.transform.position, midPoint3.transform.rotation);
-        rotate.GetComponent<MidFlip>().Flip();
+        GameObject.Instantiate(mid[Random.Range(0, mid.Length - 1)], midPoint3.transform.position, midPoint3.transform.rotation);
         GameObject.Instantiate(end[Random.Range(0, end.Length - 1)], endPoint.transform.position, endPoint.transform.rotation);
     }
 
